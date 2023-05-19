5ef89706ec2a2aacf8768080b4812a968fadc608
Bruno S Velho <bruno.velho@gmail.com>
2023-05-19 20:16:31 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline" >Hello World!!!</h1>
+    </>
+    // text-3xl font-bold underline
+  )
+}
